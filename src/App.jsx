660b8b0554187fe94b12b87147447ff6c3972387
webbdev <forrwebdev@gmail.com>
2026-04-09 23:52:02 +0100

    fix(App): add React Router v7 future flags, move router outside component

--- src/App.jsx
+++ src/App.jsx
@@ -7,24 +7,33 @@ import {
 import MainLayouts from './layouts/MainLayouts'
 import Home from './pages/Home'
 import About from './pages/About'
-import Project, {projectLoader} from './pages/Project'
+import Project, { projectLoader } from './pages/Project'
 import NotFound from './pages/NotFound'
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<MainLayouts />}>
+      <Route index element={<Home />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/:id' loader={projectLoader} element={<Project />} />
+      <Route path='/not' element={<NotFound />} />
+    </Route>
+  ),
+  {
+    future: {
+      v7_startTransition: true,
+      v7_relativeSplatPath: true,
+    },
+  }
+)
 
 const App = () => {
-
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path='/' element={<MainLayouts />}>
-        <Route index element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/:id' loader={projectLoader} element={<Project />} />
-        <Route path='/not' element={<NotFound />} />
-      </Route>
-    )
+  return (
+    <RouterProvider
+      router={router}
+      future={{ v7_startTransition: true }}
+    />
   )
-
-  return <RouterProvider router={router} />
 }
 
 export default App
